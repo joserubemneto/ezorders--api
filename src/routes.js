bb6f58402d43dba4adf483a8d9b05326b2e022fe
Jose Rubem <joserubemn31@gmail.com>
2020-12-20 22:48:50 +0000

    feat: use Order controller

--- src/routes.js
+++ src/routes.js
@@ -2,9 +2,10 @@ const { Router} = require('express')
 
 const router = Router()
 
+const OrderController = require('./controllers/OrderController')
 
-router.get('/orders')
-router.post('/orders')
-router.patch('/orders/:id/status')
+router.get('/orders', OrderController.index)
+router.post('/orders', OrderController.store)
+router.patch('/orders/:id/status', OrderController.update)
 
 module.exports = router
