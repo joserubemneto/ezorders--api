5b285a83d080bf58dddd060a0a28b29e1759cf64
Jose Rubem <joserubemn31@gmail.com>
2020-12-20 22:52:50 +0000

    chore: added express body parser

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ const express = require('express')
 const routes = require('./routes')
 
 const app = express()
+app.use(express.json())
 app.use(routes)
 
 app.listen(3001, () => console.log('> Server started at http://localhost:3001'))
