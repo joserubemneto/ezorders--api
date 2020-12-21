6bb916a9b2d9fed22a7eeecebec306b47a96aa31
Jose Rubem <joserubemn31@gmail.com>
2020-12-21 00:43:46 +0000

    feat: added socketIo setup

--- src/index.js
+++ src/index.js
@@ -1,6 +1,10 @@
+const { request } = require('express')
 const express = require('express')
 const mongoose = require('mongoose')
 const routes = require('./routes')
+const http = require('http')
+const socketIo = require('socket.io')
+
 
 mongoose.connect('mongodb://localhost:2717/ezorders', {
   useNewUrlParser: true,
@@ -9,7 +13,10 @@ mongoose.connect('mongodb://localhost:2717/ezorders', {
 })
 
 const app = express()
+const server = http.Server(app)
+const io = socketIo(server)
+
 app.use(express.json())
 app.use(routes)
 
-app.listen(3001, () => console.log('> Server started at http://localhost:3001'))
+server.listen(3001, () => console.log('> Server started at http://localhost:3001'))
