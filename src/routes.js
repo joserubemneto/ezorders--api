d13231462d0f9770e673cd10e61ae90555de1136
Jose Rubem <joserubemn31@gmail.com>
2021-02-15 02:04:05 +0000

    feat: added delete order route

--- src/routes.js
+++ src/routes.js
@@ -1,11 +1,12 @@
-const { Router} = require('express')
+const { Router } = require("express");
 
-const router = Router()
+const router = Router();
 
-const OrderController = require('./controllers/OrderController')
+const OrderController = require("./controllers/OrderController");
 
-router.get('/orders', OrderController.index)
-router.post('/orders', OrderController.store)
-router.patch('/orders/:id/status', OrderController.update)
+router.get("/orders", OrderController.index);
+router.post("/orders", OrderController.store);
+router.patch("/orders/:id/status", OrderController.update);
+router.delete("/orders", OrderController.delete);
 
-module.exports = router
+module.exports = router;
