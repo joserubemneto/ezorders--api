b4fd664dff5054bf998364c925d3ce33a63ac683
Jose Rubem <joserubemn31@gmail.com>
2021-02-16 22:48:03 +0000

    chore: added heroku deploy configs

--- src/index.js
+++ src/index.js
@@ -27,6 +27,6 @@ app.use(cors());
 app.use(express.json());
 app.use(routes);
 
-server.listen(3001, () =>
-  console.log("> Server started at http://localhost:3001 ok")
+server.listen(process.env.PORT || 3001, () =>
+  console.log("> Server is running")
 );
