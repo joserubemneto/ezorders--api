659656d84117532e57324a7d24db54c528569bb9
Jose Rubem <joserubemn31@gmail.com>
2021-02-16 19:44:05 +0000

    refactor: update docker connection

--- src/index.js
+++ src/index.js
@@ -1,28 +1,29 @@
-const { request, response } = require('express')
-const express = require('express')
-const mongoose = require('mongoose')
-const routes = require('./routes')
-const http = require('http')
-const socketIo = require('socket.io')
-const cors = require('cors')
+const { request, response } = require("express");
+const express = require("express");
+const mongoose = require("mongoose");
+const routes = require("./routes");
+const http = require("http");
+const socketIo = require("socket.io");
+const cors = require("cors");
 
-
-mongoose.connect('mongodb://localhost:2717/ezorders', {
+mongoose.connect("mongodb://localhost:27017/ezorders", {
   useNewUrlParser: true,
   useUnifiedTopology: true,
   useFindAndModify: false,
-})
+});
 
-const app = express()
-const server = http.Server(app)
-const io = socketIo(server)
+const app = express();
+const server = http.Server(app);
+const io = socketIo(server);
 
 app.use((request, response, next) => {
-  request.io = io
-  next()
-})
-app.use(cors())
-app.use(express.json())
-app.use(routes)
+  request.io = io;
+  next();
+});
+app.use(cors());
+app.use(express.json());
+app.use(routes);
 
-server.listen(3001, () => console.log('> Server started at http://localhost:3001'))
+server.listen(3001, () =>
+  console.log("> Server started at http://localhost:3001")
+);
